57c2cff11c95dbd5e45054a7ebf7f74aac305cd7
m1sk9 <me@m1sk9.dev>
2024-10-14 15:48:32 +0900

    style: Apply font

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,18 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>m1s.dev</title>
         <link rel="stylesheet" href="/styles.css" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        >
+        </link>
       </head>
       <body>
         <Component />
