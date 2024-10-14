d3b846ff6200a8687b5a17c67c799a0aef110909
m1sk9 <me@m1sk9.dev>
2024-10-15 05:41:22 +0900

    ci: fix trigger

--- src/component/organisms/Itemization.tsx
+++ src/component/organisms/Itemization.tsx
@@ -1,37 +1,37 @@
 export interface ItemizationProps {
-    name: string;
-    description: string;
-    src: string;
-    date: string;
-    isArchive?: boolean;
+  name: string;
+  description: string;
+  src: string;
+  date: string;
+  isArchive?: boolean;
 }
 
 export default function Itemization({ items }: { items: ItemizationProps[] }) {
-    return (
-        <div>
-            {items.map((i, k) => (
-                <details key={k}>
-                    <summary>
-                        {i.name} ({i.date})
-                        {i.isArchive && (
-                            <span class="text-gray-500 ml-4 text-sm">
-                                Archive / Not Maintained
-                            </span>
-                        )}
-                    </summary>
-                    <ul class="ml-8">
-                        {i.description}
-                    </ul>
-                    <a
-                        href={i.src}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        class="ml-8"
-                    >
-                        {i.src}
-                    </a>
-                </details>
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      {items.map((i, k) => (
+        <details key={k}>
+          <summary>
+            {i.name} ({i.date})
+            {i.isArchive && (
+              <span class="text-gray-500 ml-4 text-sm">
+                Archive / Not Maintained
+              </span>
+            )}
+          </summary>
+          <ul class="ml-8">
+            {i.description}
+          </ul>
+          <a
+            href={i.src}
+            target="_blank"
+            rel="noopener noreferrer"
+            class="ml-8"
+          >
+            {i.src}
+          </a>
+        </details>
+      ))}
+    </div>
+  );
 }
